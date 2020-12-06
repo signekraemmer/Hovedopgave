1dc84089da6963f1a5ee77097737701c39d9a4c5
carolinesieck <54636707+carolinesieck@users.noreply.github.com>
2020-12-06 23:46:12 +0100

    j

---
+++ style.js
@@ -0,0 +1,9 @@
+document.getElementsByClassName("clickEvent").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementsByClassName("clickEvent").style.color = "#3c505e"
+
+  document.getElementsByClassName("clickEvent").style.border = "#3c505e solid 1px"
+
+  document.getElementsByClassName("clickEvent").style.backgroundColor = "#ffffff"
+}
