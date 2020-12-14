25d48efff6379b0b0bc49bd060c26f6d8be476ef
carolinesieck <54636707+carolinesieck@users.noreply.github.com>
2020-12-14 20:43:30 +0100

    Justeret

--- style.js
+++ style.js
@@ -1,15 +1,32 @@
-const btn = document.querySelector('.clickEvent')
 
-btn.addEventListener('click', function(){
-  btn.style.backgroundColor = '#fff';
-  btn.style.border = '#3c505e solid 2px';
-  btn.style.color = '#3c505e';
-  btn.style.transition = 'all 0.7s';
 
-  setTimeout(function(){
-    btn.style.backgroundColor = '#3c505e';
-    btn.style.border = '#3c505e solid 2px';
-    btn.style.color = '#fff';
+function colorChange() {
+    document.getElementById("clickEvent").style.backgroundColor = "#fff";
+    document.getElementById("clickEvent").style.border = "#3c505e solid 2px";
+    document.getElementById("clickEvent").style.color = "#3c505e";
+    document.getElementById("clickEvent").style.transition = "all 0.7s";
 
-  }, 700)
-})
+
+    setTimeout(function(){
+        document.getElementById("clickEvent").style.backgroundColor = "#3c505e";
+        document.getElementById("clickEvent").style.border = "#3c505e solid 2px";
+        document.getElementById("clickEvent").style.color = "#fff";
+
+    },700);
+  }
+
+
+  function colorSwitch() {
+      document.getElementById("click").style.backgroundColor = "#fff";
+      document.getElementById("click").style.border = "#3c505e solid 2px";
+      document.getElementById("click").style.color = "#3c505e";
+      document.getElementById("click").style.transition = "all 0.7s";
+
+
+      setTimeout(function(){
+          document.getElementById("click").style.backgroundColor = "#3c505e";
+          document.getElementById("click").style.border = "#3c505e solid 2px";
+          document.getElementById("click").style.color = "#fff";
+
+      },700);
+    }
